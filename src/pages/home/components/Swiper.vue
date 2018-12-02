48f6e4d644cfb66b796c189a0d953bf16892e507
congleiwang <congleiwang@qq.com>
2018-12-03 00:17:45 +0800

    swiper code done

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for='item of swiperList' :key="item.id">
+        <img class='img-swiper' :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+      <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+      <!--<div class="swiper-button-next" slot="button-next"></div>-->
+      <!--<div class="swiper-scrollbar" slot="scrollbar"></div>-->
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: {
+          stopOnLastSlide: false
+        },
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [{
+        id: 1,
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/61/44400d6591891202.jpg_750x200_7565a05c.jpg'
+      }, {
+        id: 2,
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/7c/8e5c4ab8ee8b7402.jpg_750x200_dd7a38dd.jpg'
+      }, {
+        id: 3,
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/b7/1e9c4ca462741f02.jpg_750x200_842d5261.jpg'
+      }, {
+        id: 4,
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/a8/cb43c4ac6c215d02.jpg_750x200_83dee994.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  /*直接穿透入swiper组件*/
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #ffffff
+
+  .wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 26.66667%
+    background-color: #eeeeee
+
+    .img-swiper
+      width: 100%
+</style>
