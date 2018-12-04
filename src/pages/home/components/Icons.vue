f58312d39719a759832ff45fc67fd7a0e6c3e563
congleiwang <congleiwang@qq.com>
2018-12-05 00:55:01 +0800

    finish icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="icons">
+    <swiper >
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icons-icon" v-for="item of page" :key="item.id">
+          <div class="icons-icon-img">
+            <img class="icons-icon-img-content" :src='item.imgUrl' />
+          </div>
+          <p class="icons-icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '广州必游'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '动植物园'
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+        desc: '游乐场'
+      }, {
+        id: '005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+        desc: '珠江夜游'
+      }, {
+        id: '006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+        desc: '泡温泉'
+      }, {
+        id: '007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/5e/c640ec3b7d7ae802.png',
+        desc: '打卡圣地'
+      }, {
+        id: '009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+        desc: '玩转长隆'
+      }, {
+        id: '010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+        desc: '全部玩乐'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (pages[page] == null) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+  .icons
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    background-color #ffffff
+
+    .icons-icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+
+      .icons-icon-img
+        position: absolute
+        left: 0
+        top: 0
+        right: 0
+        bottom: .44rem
+        -webkit-box-sizing: border-box
+        -moz-box-sizing: border-box
+        box-sizing: border-box
+        padding: .1rem
+
+        .icons-icon-img-content
+          display: block
+          margin: 0 auto
+          height: 90%
+
+      .icons-icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        line-height: .44rem
+        height: .44rem
+        color: $darkTextColor
+        text-align: center
+        ellipsis ()
+</style>
